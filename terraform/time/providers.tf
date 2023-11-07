2be79582102bdc396844850043b238911d103f84
Hunter Williams <hunter@westwilliams.com>
2023-11-07 03:03:31 -0500

    Update

--- terraform/time/providers.tf
+++ terraform/time/providers.tf
@@ -1,16 +1,10 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.0"
-    }
     time = {
       source  = "hashicorp/time"
       version = "0.9.1"
     }
   }
 }
-
-provider "random" {}
 
 provider "time" {}
